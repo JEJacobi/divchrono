5d817c406adc5e31b954499480ef30950e88cbbe
Logan A <LoganTAlexander@gmail.com>
2016-10-05 01:16:13 +0000

    Relocates GPIO initalization and cleanup to pins.py.

--- src/pins.py
+++ src/pins.py
@@ -1,8 +1,9 @@
-"""Settings for all input and output pins used by other modules.
+"""Module for handling GPIO pin setup and settings.
 
-Uses RPi.GPIO.
+Uses RPi.GPIO
 """
 import RPi.GPIO as GPIO
+
 mode = GPIO.BCM # Numbering system
 
 status_data = 16 # For status LED shift register.
@@ -10,3 +11,18 @@ status_clock = 20
 status_latch = 21
 
 outputs = [status_data, status_clock, status_latch]
+inputs = []
+
+def init():
+    GPIO.setwarnings(False)
+    GPIO.setmode(mode)
+
+    for x in outputs:
+        GPIO.setup(x, GPIO.OUT)
+
+    for x in inputs:
+        GPIO.setup(x, GPIO.IN)
+
+def destroy():
+    """Clean up, stop using GPIO pins."""
+    GPIO.cleanup()
